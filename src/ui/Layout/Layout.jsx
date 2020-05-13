967a1b018c3d1a55757864e8cf3a5d1297039e4c
Andreas Gasser <gasserandreas@users.noreply.github.com>
2020-05-13 20:14:10 +0200

    feat(#25): disabled app for small devices

--- src/ui/Layout/Layout.jsx
+++ src/ui/Layout/Layout.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles(() => ({
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+
+import Footer from '../Footer/Footer';
+
+const useStyles = makeStyles((theme) => ({
   layout: {
     flexGrow: 1,
     flexShrink: 1,
+  },
+  notSupported: {
+    [theme.breakpoints.up('sm')]: {
+      display: 'none',
+    },
+
+    height: '100vh',
+    display: 'flex',
+    flexFlow: 'column',
+    justifyContent: 'space-between',
+    textAlign: 'center',
+    padding: '4rem 2rem 1rem',
   },
 }));
 
@@ -14,6 +31,17 @@ const Layout = ({ children, ...props }) => {
 
   return (
     <main className={classes.layout} {...props}>
+      <Box className={classes.notSupported} justifyContent="space-between">
+        <Box>
+          <Typography variant="h1" gutterBottom>Not supported</Typography>
+          <Typography>
+            I am sorry, your device is not supported.
+            <br />
+            Please use another device and try again.
+          </Typography>
+        </Box>
+        <Footer />
+      </Box>
       {children}
     </main>
   );
