27d10de13aca2fc534c1dde0cf6dafd90fd0ae77
Andreas Gasser <gasserandreas@users.noreply.github.com>
2020-05-19 23:21:41 +0200

    feat(#46): added HotKeys library

--- src/ui/Layout/Page.jsx
+++ src/ui/Layout/Page.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { withHotKeys } from 'react-hotkeys';
+
 import { makeStyles } from '@material-ui/core/styles';
 
 import clsx from 'clsx';
@@ -11,11 +13,13 @@ const useStyles = makeStyles(() => ({
   },
 }));
 
-const Page = ({ children, className, ...props }) => {
+const Page = ({
+  children, className, hotKeys, ...props
+}) => {
   const classes = useStyles();
 
   return (
-    <div className={clsx(classes.page, className)} {...props}>
+    <div className={clsx(classes.page, className)} {...hotKeys} {...props}>
       {children}
     </div>
   );
@@ -24,10 +28,36 @@ const Page = ({ children, className, ...props }) => {
 Page.propTypes = {
   children: PropTypes.node.isRequired,
   className: PropTypes.string,
+  handlers: PropTypes.shape({}),
+  hotKeys: PropTypes.any, // eslint-disable-line react/forbid-prop-types
 };
 
 Page.defaultProps = {
   className: '',
+  handlers: null,
+  hotKeys: null,
 };
 
-export default Page;
+export default withHotKeys(Page);
+
+
+// const HOCPage = ({ handlers, ...props }) => {
+//   if (!handlers) {
+//     return <Page {...props} />;
+//   }
+
+//   return withHotKeys(<Page handlers={handlers} {...props} />);
+// };
+
+// HOCPage.propTypes = {
+//   children: PropTypes.node.isRequired,
+//   className: PropTypes.string,
+//   handlers: PropTypes.shape({}),
+// };
+
+// HOCPage.defaultProps = {
+//   className: '',
+//   handlers: null,
+// };
+
+// export default HOCPage;
