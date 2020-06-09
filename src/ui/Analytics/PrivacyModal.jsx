0bb1da33fe25b4f496ce74d53c797ab07389af36
Andreas Gasser <gasserandreas@users.noreply.github.com>
2020-06-09 11:42:57 +0200

    chore: minor style fix

--- src/ui/Analytics/PrivacyModal.jsx
+++ src/ui/Analytics/PrivacyModal.jsx
@@ -16,7 +16,6 @@ const useStyles = makeStyles((theme) => ({
   },
   paper: {
     backgroundColor: theme.palette.background.paper,
-    border: '2px solid #000',
     boxShadow: theme.shadows[5],
     padding: theme.spacing(2, 4, 3),
     maxWidth: '60%',
