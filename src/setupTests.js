02a497f77fe2d587fc5a438f480106ab8658e3ab
Andreas Gasser <gasserandreas@users.noreply.github.com>
2021-03-28 18:27:04 +0200

    task(#101): fixed test issues

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,13 +1,8 @@
 /* eslint-disable */
-// import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
 import jestFetchMock from 'jest-fetch-mock';
 
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
-
-// setup enzyme
-// configure({ adapter: new Adapter() });
 
 // setup fetch-mock
 global.fetch = jestFetchMock;
